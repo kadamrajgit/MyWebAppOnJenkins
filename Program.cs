b562768276614f9a2bab8410f633859cae26335d
rajesh <kadam.raj@outlook.com>
2023-04-01 18:43:25 +0800

    testgdgd dgdgdg dgdgds

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddRazorPages();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//test
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
